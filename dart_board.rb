0588e12b7396f4e77a5ba5e7e5b79b6c87d88c6f
Sam Backus <sbackus@patientslikeme.com>
2018-01-01 14:20:51 -0500

    replace circle.rb with circle.png

--- dart_board.rb
+++ dart_board.rb
@@ -1,15 +1,21 @@
-#https://www.youtube.com/watch?v=M34TO71SKGk
+# https://www.youtube.com/watch?v=M34TO71SKGk
 
+# draw a cicle inside a square with the diameter equal to the sides of the square
+# then randomly throw darts at the target
+# the number of darts in the circle divided by the number of total darts thrown
+# should be proportional to
+# the area of the circle divided by the area of the square
+# the area of a circle is pi r^2
+# so if the area of the square is one the area of the circle is pi/4
+# so the value of pi approximately n inside / n total * 4
 
 require 'gosu'
-require 'rmagick'
-require './circle'
 
 class Dart
   attr_accessor :x, :y
   def initialize
-    @x = rand(DartBoard::DIAMETER)
-    @y = rand(DartBoard::DIAMETER)
+    @x = rand * DartBoard::DIAMETER
+    @y = rand * DartBoard::DIAMETER
   end
 
   def draw(color = Gosu::Color::WHITE)
@@ -24,20 +30,23 @@ class Dart
 end
 
 class DartBoard < Gosu::Window
-  DIAMETER = 900
+  DIAMETER = 678
   RADIUS = DIAMETER/2
   def initialize
     super DIAMETER, DIAMETER
     self.caption = "Dart Board Simulation to calculate pi"
-    @circle = Gosu::Image.new(self, Circle.new(RADIUS, 234, 111, 100), false)
+    @circle = Gosu::Image.new('assets/circle.png')
     @darts = []
     @inside_count = 0
     @outside_count = 0
-    @font = Gosu::Font.new(50)
+    @font_size = 50
+    @font = Gosu::Font.new(@font_size)
   end
 
   def update
-    throw_dart
+    5.times do
+      throw_dart
+    end
     estimate_pi
   end
 
@@ -47,12 +56,12 @@ class DartBoard < Gosu::Window
       if dart.inside(RADIUS)
         dart.draw(Gosu::Color::RED)
       else
-        dart.draw(Gosu::Color::GREEN)
+        dart.draw(Gosu::Color::BLUE)
       end
     end
     @font.draw("inside: #{@inside_count}", 0,0,0)
-    @font.draw("total: #{@darts.length}", 0,50,0)
-    @font.draw("Pi: #{@pi}", 0, 100 ,0)
+    @font.draw("total: #{@darts.length}", 0,@font_size,0)
+    @font.draw("Pi: #{@pi}", 0, @font_size * 2 ,0)
   end
 
   def estimate_pi
