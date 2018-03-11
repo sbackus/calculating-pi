4cbe303e9e5b68b49a50f2f6facb9f3c3931faed
Sam Backus <sbackus@patientslikeme.com>
2018-03-11 12:54:36 -0400

    starting points for euler and chudnovsky

--- chudnovsky.rb
+++ chudnovsky.rb
@@ -1,5 +1,3 @@
 # https://en.wikipedia.org/wiki/Chudnovsky_algorithm
 
 # just the first term gives you a dozen digits of pi
-pi = (12*13591409/640320**(3.0/2.0))**-1
-puts pi
--- euler.rb
+++ euler.rb
@@ -1,15 +1,2 @@
 # the sum of the reciprocals of the positive square integers is equal to pi squared divided by 6
 # 1/1^2 + 1/2^2 + 1/3^2 + 1/4^2 + 1/5^2 ... = pi^2 / 6
-
-n = 1000000
-
-sum = 0
-for i in 1..n
-  sum += 1.0 / i ** 2
-end
-
-pi = Math.sqrt(sum * 6)
-
-puts pi
-
-# https://www.youtube.com/watch?v=HrRMnzANHHs&t=243s
